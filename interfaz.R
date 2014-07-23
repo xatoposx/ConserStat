e62a540bf46ce60ff461021d5901a5dd8097e5e4
xatoposx <atopos@openmailbox.org>
2014-07-23 18:26:18 +0200

    New UI wrapper functions

--- interfaz.R
+++ interfaz.R
@@ -1,55 +1,72 @@
-## FUNCIONES DE INTERFAZ DE USUARIO (GENERACIÓN DE FICHEROS PARA MEMORIA)
-library(knitr)
+## FUNCIONES DE INTERFAZ DE USUARIO
 source("explorar.R")
-# !!! TODO - Support (at the inteface level) different types of latex output
-#            (desglosar, sin_desglosar).
-# !!! TODO - Rethink LaTeX output (even with '\nopagebreak', captions are
-#            still shifted to the next page).
-# !!! TODO - Generalize to avoid code repetition
-# !!! TODO - Revise png options for one-file/one-col ggplots: wrong dimensions.
-GenerarGraficosParaMemoria <- 
-function(estadistica="AprobadoPorCurso", tipoGrafico="apilado", desglosar=FALSE) {
-	# Genera imágenes PNG para la memoria y los guarda en disco
-	xy <- unlist(strsplit(estadistica, "Por"))	
+source("imprimir.R")
 
-	x <- xy[2]
-	y <- xy[1]
-	grupo <- ifelse(desglosar, "Asignatura", "Departamento")
-	subconjunto <- if (desglosar) AsignaturasEn else identity
-
-	# Construye nombre de fichero y tablas
-	nf <- lapply(departamentos,
-		     function(d) SinAcentos(paste(x, y, "_", grupo, "_", d, 
-						  "-", tipoGrafico, sep="")))
-	print(nf)
-	gg <- lapply(departamentos,
-		     function(d) Graficar(x, y, grupo, subconjunto(d),
-					  tipoGrafico=tipoGrafico))
-
-	mapply(Guardar, gg, nf)
+## Funciones de exploración
+MostrarAprobadosPorCursoEnAsignatura <- function(asignatura) {
+	# Muestra diagrama de barras y tabla en consola de aprobados/suspensos
+	# por curso en la asignatura dada
+	print(Tabular("Curso", "Aprobado", "Asignatura", asignatura))
+	Graficar("Curso", "Aprobado", "Asignatura", asignatura)
 }
 
-GenerarTablasParaMemoria <- 
-function(estadistica="AprobadoPorCurso", desglosar=FALSE) {
-	# Genera tablas LATEX para la memoria y las guarda en disco
-	xy <- unlist(strsplit(estadistica, "Por"))	
-
-	x <- xy[2]
-	y <- xy[1]
-	grupo <- ifelse(desglosar, "Asignatura", "Departamento")
-	subconjunto <- if (desglosar) AsignaturasEn else identity
-
-	# Construye nombre de fichero y tablas
-	nf <- lapply(departamentos,
-		     function(d) SinAcentos(paste(x, y, "_", grupo, 
-						  "_", d, sep="")))
-	print(nf)
-	tt <- lapply(departamentos,
-		     function(d) Tabular(x, y, grupo, subconjunto(d)))
-
-	mapply(Guardar, tt, nf)
+MostrarAprobadosPorCursoEnDepartamento <- function(departamento, desglosar=TRUE) {
+	# Muestra diagrama de barras y tabla en consola de aprobados/suspensos
+	# por curso en el departamento dado. Si desglosar=TRUE, muestra datos
+	# desglosados por asignatura dentro del departamento.
+	if (desglosar) {
+		print(Tabular("Curso", "Aprobado", "Asignatura", 
+			      AsignaturasEn(departamento)))
+		print(Graficar("Curso", "Aprobado", "Asignatura", 
+			      AsignaturasEn(departamento)))
+	} else {
+		print(Tabular("Curso", "Aprobado", "Departamento", departamento))
+		Graficar("Curso", "Aprobado", "Departamento", departamento)
+	}
 }
 
+CompararNotasPorCursoEnAsignatura <- function(asignatura) {
+	# Muestra diagramas de caja de notas por curso en la asignatura dada
+	Graficar("Curso", "Nota", "Asignatura", asignatura)
+}
+
+CompararNotasPorCursoEnDepartamento <- function(departamento, desglosar=TRUE) {
+	# Muestra diagramas de caja de notas por curso en el departamento dado.
+	# Si desglosar=TRUE muestra datos desglosados por asignatura en el
+        # departamento
+	if (desglosar)
+		Graficar("Curso", "Nota", "Asignatura", AsignaturasEn(departamento))
+	else
+		Graficar("Curso", "Nota", "Departamento", departamento)
+}
+
+ContarNotasEnAsignatura <- function(asignatura) {
+	# Muestra un histograma de cuentas de notas en la asignatura dada
+	Graficar("Nota", y=NULL, "Asignatura", asignatura)
+}
+
+ContarNotasEnDepartamento <- function(departamento, desglosar=TRUE) {
+	# Muestra un histograma de cuentas de notas en el departamento dado.
+	# Si desglosar=TRUE muestra datos desglosados por asignatura en el
+	# departamento
+	if (desglosar)
+		Graficar("Nota", y=NULL, "Asignatura", AsignaturasEn(departamento))
+	else
+		Graficar("Nota", y=NULL, "Departamento", departamento)
+}
+
+CompararDistribucionDeNotasPorCurso <- function() {
+	# Muestra gráficos de densidad de notas por curso
+	Graficar("Nota", y=NULL, "Curso", asignaturas)
+}
+
+CompararAprobadosPorCurso <- function() {
+	# Muestra gráfico mosica de aprobados/suspensos por curso.
+	mosaicplot(xtabs(~ Curso + Aprobado, data=datos), 
+		   col=c("Pink", "LightBlue"), main="")
+}
+
+## Funciones de generación de informe
 GenerarInforme <- function(nombreInforme="informe.tex", desglosar=TRUE) {
 	# Genera un informe en pdf
 
@@ -68,87 +85,5 @@ GenerarInforme <- function(nombreInforme="informe.tex", desglosar=TRUE) {
 	#knit2pdf(nombreInforme, compiler="pdflatex")
         system(command="pdflatex informe.tex")
 
-}
-
-GenerarFicheroTex <- function(fichero) {
-	# Preámbulo
-	preambulo <- ("
-		      \\documentclass[a4paper]{article}
-		      \\usepackage[T1]{fontenc}
-		      \\usepackage[utf8]{inputenc}
-		      \\usepackage[spanish]{babel}
-		      \\usepackage{graphicx}
-                      \\usepackage{capt-of}
-		      \\usepackage{booktabs}\n
-		      \\renewcommand{\\arraystretch}{1.3}
-		      \\pagestyle{empty}\n
-		      \\title{Informe}
-		      \\begin{document}
-		      \\maketitle
-		      ")
-	preambulo <- gsub("[\t ]*", "", preambulo)
-
-	# Sección tablas 
-	tituloTablas <- ("\\section{Tablas}\n")
-	tablas <- unlist(lapply(dir("./tablas"), GenerarSeccion, 
-				tipoSeccion="subsection*", tipoFichero="tabla"))
-
-	# Sección gráficos
-	tituloGraficos <- ("\\section{Gráficos}\n")
-	graficos <- unlist(lapply(dir("./graficos"), GenerarSeccion, 
-				  tipoSeccion="subsection*", tipoFichero="imagen"))
-
-	# Final del documento
-	final <- "\\end{document}"
-
-	cat(preambulo, "\n", tituloTablas, tablas, "\n", 
-	    tituloGraficos, graficos, final, sep="", file=fichero)
-}
-
-GenerarSeccion <- function(fichero, tipoSeccion, tipoFichero) {
-	# Produce titulo de sección y etiqueta '\input' por fichero que incluir
-	# Asumo: formato nombre fichero =
-	#        estadistica_Grupo_Departamento_<*>.extension
-	nombreBase <- ifelse(grepl("-", fichero),
-			     strsplit(fichero, split="-")[[1]][1],
-			     strsplit(fichero, split="\\.")[[1]][1]) 
-	nombreDpto <- sub("([[:alpha:]]+_){2}", "", nombreBase)
-
-	tituloSeccion <- paste("\\", tipoSeccion, "{", SinGuiones(nombreDpto), "}", sep="")
-
-	tag <- ifelse(tipoFichero == "imagen", 
-		      "\\includegraphics[scale=0.5]", 
-		      "\\input")
-	if (tipoFichero == "imagen")
-		dir <- "graficos"
-
-	if (tipoFichero == "tabla")
-		dir <- "tablas"
-
-	inclusionFichero <- paste(tag, "{", file.path(dir, fichero), "}", sep="")
-
-	paste(tituloSeccion, "\n", inclusionFichero, "\n", sep="")
-}
-
-TablasNoFlotantes <- function(fichero) {
-	# Elimina tablas flotantes del contenido del fichero pero mantiene título
-	#
-	# [Es más fácil modificar aquí las cosas con este hack. La razón
-	# es que pdflatex produce "too many floats" si el documento contiene
-	# muchas tablas flotantes. Sin embargo es necesario
-	# mantener la 'caption' de las tablas generadas por xtable para
-	# convertirla en 'caption' de los entornos 'tabular'.
-	# Esto sólo es posible en la actual implementación dejando que
-	# print.xtable (función 'Guardar') genere flotantes en primera
-	# instancia y modificando el resultado para el caso de un documento
-	# que contiene muchas tablas como es el requerido para el informe.]
-	lineas <- readLines(fichero)
-
-	lineas <- gsub("(.*)\\{table\\}.*", "\\\\vspace{1em}\n\\1{center}", lineas)
-        lineas <- gsub("\\\\centering", "", lineas)
-	lineas <- gsub("caption", "captionof{table}", lineas)
-	lineas <- gsub("(\\\\captionof.*)", "\\\\nopagebreak[4]\\1", lineas)
-
-	writeLines(lineas, fichero)
 }
 
